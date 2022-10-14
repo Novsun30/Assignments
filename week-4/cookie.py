a30dba978ff25d94cf269cab1c9b08e53f26d2e4
Novsun30 <107986642+Novsun30@users.noreply.github.com>
2022-10-15 03:00:35 +0800

    Add files via upload

--- week-4/cookie.py
+++ week-4/cookie.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
-
+from cryptography.fernet import Fernet
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+key = Fernet.generate_key()
+f=Fernet(key)
+
 
 @app.route("/signin",methods=["POST"])
 def signIn():
@@ -12,7 +15,8 @@ def signIn():
     password = request.form["pass"]
     if account == "test" and password == "test":
         res = make_response(redirect("/member"))
-        res.set_cookie(key="login", value="True")
+        valueT = f.encrypt(b"True")
+        res.set_cookie(key="login", value=valueT)
         return res
     elif account == "" or password == "":
         return redirect(url_for("error",message=["請輸入帳號、密碼"]))
@@ -21,15 +25,17 @@ def signIn():
 @app.route("/signout")
 def signOut():
     res = make_response(redirect("/"))
-    res.set_cookie(key="login", value="False")
+    res.set_cookie(key="login", expires=0)
     return res
 
 @app.route("/member")
 def member():
     login = request.cookies.get("login")
-    if login == "True":
-        return render_template("member.html")
-    else:
+    try:
+        loginValue = f.decrypt(login).decode("ascii")
+        if loginValue == "True":
+            return render_template("member.html")
+    except:
         return redirect("/")
 
 @app.route("/error")
