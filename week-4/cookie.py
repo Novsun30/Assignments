9a9c0f634274d70176ab828b942639ae6dd38769
Novsun30 <107986642+Novsun30@users.noreply.github.com>
2022-10-14 18:18:03 +0800

    Add files via upload

--- week-4/cookie.py
+++ week-4/cookie.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 
 app = Flask(__name__)
 app.secret_key="You know nothing, Jon Snow"
@@ -12,27 +12,27 @@ def signIn():
     account = request.form["acc"]
     password = request.form["pass"]
     if account == "test" and password == "test":
-        session["login"]=True
-        return redirect("/member")
+        res = make_response(redirect("/member"))
+        res.set_cookie(key="login", value="True")
+        return res
     elif account == "" or password == "":
         return redirect(url_for("error",message=["請輸入帳號、密碼"]))
     return redirect(url_for("error",message=["帳號、或密碼輸入錯誤"]))
 
 @app.route("/signout")
 def signOut():
-    session['login'] = False
-    return redirect("/")
+    res = make_response(redirect("/"))
+    res.set_cookie(key="login", value="False")
+    return res
 
 @app.route("/member")
 def member():
-    try:
-        if session["login"] == True:
-            return render_template("member.html")
-        elif session["login"] == False:
-            return redirect("/")
-    except:
+    login = request.cookies.get("login")
+    if login == "True":
+        return render_template("member.html")
+    else:
         return redirect("/")
-    
+
 @app.route("/error")
 def error():
     return render_template("error.html", showMessage=request.args.get("message"))
